cd837cebda7fcb00d257cbd669556c9cea6cdfe7
Joel Feiner <joel@warpwire.net>
2019-03-13 10:33:45 -0400

    Restructured into 5 plugins according to Moodle plugin standards

---
+++ version.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Atto text editor integration version file.
+ *
+ * @package    atto_warpwire
+ * @copyright  2016 Warpwire, Inc.  <warpwire.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'atto_warpwire';
+$plugin->version   = 2019030600;
+$plugin->release   = '3.0.8';
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->requires  = 2015111607;
+
+$plugin->dependencies = array(
+    'local_warpwire' => 2019030600
+);
